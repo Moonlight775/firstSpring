42cb4f6ecc80a90b9d8a9297c37672191ed10fce
KwanYoung Moon <pianoguys775@gmail.com>
2022-01-24 22:36:47 +0900

    스프링 - reflection API - instance, static method

--- mvc/src/main/java/com/moon775/mvc/Hello.java
+++ mvc/src/main/java/com/moon775/mvc/Hello.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 // 1. 원격 호출가능한 프로그램으로등록
 @Controller
 public class Hello {
+	int iv = 10; // 인스턴스 변수
+	static int cv = 20; // static 변수
+	
 	// 2. URL과 메서드를 연결
 	@RequestMapping("/hello")
-	public void main() {
-		System.out.println("Hello");
+	private void main() {	// 인스턴스 메서드 - iv, cv를 둘다 사용가능
+		System.out.println("Hello - private");
+		System.out.println(cv);	// OK
+		System.out.println(iv); // OK
 	}
+	
+	 public static void main2() {	// static 메서드 - cv만 사용가능
+		 System.out.println(cv); // OK
+//		 System.out.println(iv); // error
+		 
+	 }
 }
---
+++ mvc/src/main/java/com/moon775/mvc/Main.java
@@ -0,0 +1,22 @@
+package com.moon775.mvc;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+//		Hello hello = new Hello();
+//		hello.main();
+		
+		// Reflection API를 사용 - 클래스 정보를 얻고 다룰 수 있는 강력한 기능제공
+		// java.lang.reflct패키지를 제공
+		// Hello 클래스의 Class객체(클래스의정보를 담고 있는 객체)를 얻어온다.
+		Class helloClass = Class.forName("com.moon775.mvc.Hello");
+		Hello hello = (Hello)helloClass.newInstance(); // Class객체가 가진 정보로 객체 생성
+		Method main = helloClass.getDeclaredMethod("main");
+		main.setAccessible(true); // private인 main()을 호출가능하게 한다.
+		
+		main.invoke(hello); // hello.main()
+	}
+
+}
