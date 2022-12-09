009f400d99f72e71e69770c347f2942d80d8b91e
KwanYoung Moon <pianoguys775@gmail.com>
2022-12-09 21:23:39 +0900

    ModelAndView가 아닌 Model을 따로 써준 후 return을 view로 변경

--- mvc/src/main/java/com/moon775/mvc/YoilTellerMVC.java
+++ mvc/src/main/java/com/moon775/mvc/YoilTellerMVC.java
@@ -4,38 +4,31 @@ import java.io.IOException;
 import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 // 년월일을 입력하면 요일을 알려주는 프로그램
 @Controller
 public class YoilTellerMVC {
 	@RequestMapping("/getYoilMVC")
-	public ModelAndView main(int year, int month, int day) throws IOException{
-//		1. ModelAndView를 생성하고, 기본 뷰를 지정
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("yoilError");	// 뷰의 이름을 지정
+	public String main(int year, int month, int day, Model m) throws IOException{
 		
-//		2. 유효성 검사
+//		1. 유효성 검사
 		if(!isValid(year, month, day)) {
-			return mv;
+			return "yoilError";
 		}
 		
-//		3. 요일 계산
+//		2. 요일 계산
 		char yoil = getYoil(year, month, day);
 		
-//		4. ModelAndView에 작업한 결과를 저장
-		mv.addObject("year", year);
-		mv.addObject("month", month);
-		mv.addObject("day", day);
-		mv.addObject("yoil", yoil);
+//		3. 계산한 결과를 model에 저장
+		m.addAttribute("year", year);
+		m.addAttribute("month", month);
+		m.addAttribute("day", day);
+		m.addAttribute("yoil", yoil);
 		
-//		5. 결과를 보여줄 view를 지정
-		mv.setViewName("yoil");
 		
-//		6. ModelAndView를 반환
-		return mv;
+		return "yoil";	// /WEB-INF/views/yoil.jsp
 	}
 
 	private static boolean isValid(int year, int month, int day) {
