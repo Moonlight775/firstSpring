d968bae59841d054c1547b00c7da35829417921f
KwanYoung Moon <pianoguys775@gmail.com>
2022-12-25 22:27:55 +0900

    서블릿 필터 : 공통적인 요청 전처리와 응답 후처리에 사용 - 서블릿 호출 시의 소요시간을 알아보는 예시 작성

---
+++ mvc/src/main/java/com/moon775/mvc/PerformanceFilter.java
@@ -0,0 +1,41 @@
+package com.moon775.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+// @WebFilter = 필터등록 // urlPatterns = 필터를 적용할 요청의 패턴 지정 // "/*" = 모든 요청에 필터를 적용.
+@WebFilter(urlPatterns="/*")
+public class PerformanceFilter implements Filter {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// 초기화 작업
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// 1. 전처리 작업
+		long startTime = System.currentTimeMillis();
+
+		// 2. 서블릿 또는 다음 필터를 호출
+		chain.doFilter(request, response); 
+		
+		// 3. 후처리 작업
+		System.out.print("["+((HttpServletRequest)request).getRequestURI()+"]");
+		System.out.println(" 소요시간="+(System.currentTimeMillis()-startTime)+"ms");
+	}
+
+	@Override
+	public void destroy() {
+		// 정리 작업
+	}
+
+}
