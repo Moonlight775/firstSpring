401ab5477770c31e83de98c2564c66e243316bbc
KwanYoung Moon <pianoguys775@gmail.com>
2023-01-19 21:07:01 +0900

    예외처리 테스트 - 발생한 예외와 메시지를 확인하기 위해 error.jsp 생성 - @ExceptionHandler는 해당 Controller 내에서만 사용가능

---
+++ mvc/src/main/java/com/moon775/mvc/ExceptionController.java
@@ -0,0 +1,35 @@
+package com.moon775.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExceptionController {
+	
+	// main()과 main2()에서 중복된 try catch문 발생
+	// 코드 중복 방지위해 별도의 예외처리해주는 메소드를 추가 후 @ExceptionHandler(예외처리할 종류)를 붙여줌
+	@ExceptionHandler(Exception.class)
+	public String catcher (Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public String catcher2 (Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	
+	@RequestMapping("/ex")
+	// 매개변수로 Model을 쓴다고 해도 catcher(Model m)의 model과는 별개의 객체임, model을 통해 데이터전달X
+	public String main() throws Exception {
+		throw new Exception("예외가 발생했습니다.");
+	}
+	
+	@RequestMapping("/ex2")
+	public String main2() throws Exception {
+		throw new NullPointerException("예외가 발생했습니다.");
+	}
+}
