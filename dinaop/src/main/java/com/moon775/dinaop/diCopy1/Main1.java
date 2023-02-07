92275f27d8cc8477c913ea6ec4671b801de02d97
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-07 19:37:19 +0900

    변경에 유리한 코드 생성 (Map과 외부파일) - 프로그램 변경 없이 config.txt만 변경해주면 됨 - 매개변수를 받아서 p.getProperty()에 넣어줌으로 여러 클래스 사용가능하도록 변경

--- dinaop/src/main/java/com/moon775/dinaop/diCopy1/Main1.java
+++ dinaop/src/main/java/com/moon775/dinaop/diCopy1/Main1.java
@@ -6,19 +6,31 @@ import java.util.Properties;
 class Car {}
 class SportsCar extends Car{}
 class Truck extends Car{}
+class Engine {}
 
 public class Main1 {
     public static void main(String[] args) throws Exception{
-        Car car = getCar();
+        Car car = (Car) getObject("car");
+        Engine engine = (Engine) getObject("engine");
         System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
     }
 
-    static Car getCar() throws Exception{
+    static Object getObject(String key) throws Exception{
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
+    }
+
+    /*static Car getCar() throws Exception{
         Properties p = new Properties();
         p.load(new FileReader("config.txt"));
 
         Class clazz = Class.forName(p.getProperty("car"));
 
         return (Car) clazz.newInstance();
-    }
+    }*/
 }
