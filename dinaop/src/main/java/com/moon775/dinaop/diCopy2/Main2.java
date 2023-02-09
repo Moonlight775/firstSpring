bd1c37a3fdbc2e73faf1b3a30c775d30fb47f522
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-09 18:00:11 +0900

    하드코딩 부분 변경 - Properties와 map을 사용

--- dinaop/src/main/java/com/moon775/dinaop/diCopy2/Main2.java
+++ dinaop/src/main/java/com/moon775/dinaop/diCopy2/Main2.java
@@ -1,6 +1,7 @@
 package com.moon775.dinaop.diCopy2;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -14,9 +15,22 @@ class AppContext {
     Map map;    // 객체 저장소
 
     AppContext() {
-        map = new HashMap();
-        map.put("car", new SportsCar());
-        map.put("engine", new Engine());
+        try {
+            Properties p = new Properties();
+            p.load(new FileReader("config.txt"));
+
+            // Properties에 저장된 내용을 Map에 저장
+            map = new HashMap(p);
+            
+            // 반복문으로 클래스 이름을 얻어서 객체를 생성해서 다시 map에 저장
+            for (Object key : map.keySet()) {
+                Class clazz = Class.forName((String) map.get(key));
+                map.put(key, clazz.newInstance());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     Object getBean(String key) {
