dc3d7424a48ce00cdb5c186e1a32619306a30b27
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-10 19:51:23 +0900

    자동 객체 등록 - Component Scanning - Guava (Google Core Libraries For Java) 라이브러리 이용하기 때문에 추가 - plugin 에러로 인해 plugin dependency 추가 - 패키지내의 클래스 목록 중 @Component가 붙어 있으면 map에 저장 - config.txt 수정 필요 X - 객체 등록 위해 클래스 앞에 @Component 붙여주면 됨

---
+++ dinaop/src/main/java/com/moon775/dinaop/diCopy3/Main3.java
@@ -0,0 +1,64 @@
+package com.moon775.dinaop.diCopy3;
+
+import com.google.common.reflect.ClassPath;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Component class Car {}
+@Component class SportsCar extends Car{}
+@Component class Truck extends Car{}
+@Component class Engine {}
+
+class AppContext {
+    Map map;    // 객체 저장소
+
+    AppContext() {
+       map = new HashMap();
+       doComponentScan();
+    }
+
+    private void doComponentScan() {
+        try {
+            // 1. 패키지내의 클래스 목록을 가져온다.
+            // 2. 반복문으로 클래스를 하나씩 읽어와서 @Component 가 붙어 있는지 확인
+            // 3. @Component 가 붙어있으면 객체를 생성해서 map 에 저장
+
+            ClassLoader classLoader = AppContext.class.getClassLoader();
+            ClassPath classPath = ClassPath.from(classLoader);
+
+            // 클래스 목록(패키지내의)을 가져옴
+            Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.moon775.dinaop.diCopy3");
+
+            for (ClassPath.ClassInfo classInfo : set) {
+                Class clazz = classInfo.load();
+                Component component = (Component) clazz.getAnnotation(Component.class);
+                if (component != null) {
+                    String id = StringUtils.uncapitalize(classInfo.getSimpleName());
+                    map.put(id, clazz.newInstance());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Object getBean(String key) {
+        return map.get(key);
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) throws Exception{
+        AppContext ac = new AppContext();
+        Car car = (Car) ac.getBean("car");
+        Engine engine = (Engine) ac.getBean("engine");
+        System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+    }
+
+}
