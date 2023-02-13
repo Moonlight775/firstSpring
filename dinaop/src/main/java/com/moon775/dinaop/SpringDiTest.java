ab560b4b8455b16758d3f6dcfff30b1b1cfb7a0f
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-13 20:06:14 +0900

    component-scan 태그 사용으로 @Component와 @Autowired 어노테이션 사용 - 패키지내에 같은 이름의 class들이 다수 있어서 exclude-filter 태그로 중복되는 것들 제외 - setter 대신 @Autowired, @Value() 사용 - @Autowired는 byType, 같은 타입이 여러 개면 타입의 첫 글자를 소문자로 한 이름으로 검색  -- 이름이 매칭되는 것이 없고, 같은 타입이 여러 개라면 @Qualifier("이름")을 써주어 "이름"(key)에 맞는 객체를 연결

--- dinaop/src/main/java/com/moon775/dinaop/SpringDiTest.java
+++ dinaop/src/main/java/com/moon775/dinaop/SpringDiTest.java
@@ -1,15 +1,27 @@
 package com.moon775.dinaop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component("engine") class Engine{}     // @Conponent는 <bean id="engine" class="com.moon775.dinaop.Engine/> 와 같은 역할, ("engine")은 생략가능
+@Component class SuperEngine extends Engine{}
+@Component class TurboEngine extends Engine{}
+@Component class Door{}
+@Component
 class Car{
-    String color;
-    int oil;
-    Engine engine;
-    Door[] doors;
+    @Value("red") String color;
+    @Value("100") int oil;
+//    @Autowired Engine engine;   //  @Autowired는 byType - 타입으로 먼저 검색, 여러 개면 이름으로 검색. (타입의 첫 글자를 소문자로 한 이름으로 검색)
+
+    //    class Engine{}에 @Component가 안 붙었다고 가정하면 위의 경우 같은 타입이 여러 개 이므로 이름으로도 찾을 수 없어서 에러 발생. (engine이라는 key가 없으므로)
+    @Autowired @Qualifier("superEngine") Engine engine;    // 이럴 때 @Qualifier 에 적힌 이름(key)의 객체를 연결. @Autowired @Qualifier("superEngine")를 @Resource(name="superEngine")으로 변경 가능 (같지는 않음)
+    @Autowired Door[] doors;
 
     public Car() {} // 기본 생성자를 잊지 않고 만들어줘야 좋다.
 
@@ -35,7 +47,6 @@ class Car{
     public void setDoors(Door[] doors) {
         this.doors = doors;
     }
-
     @Override
     public String toString() {
         return "Car{" +
@@ -46,18 +57,22 @@ class Car{
                 '}';
     }
 }
-class Engine{}
-class Door{}
 
 public class SpringDiTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
 //        Car car = (Car) ac.getBean("car");  // by Name. 아래와 같은 문장
         Car car = ac.getBean("car", Car.class);  // by Name
-        Car car2 = ac.getBean(Car.class);  // by Type
+//        Car car2 = ac.getBean(Car.class);  // by Type
+//
+//        Engine engine = (Engine) ac.getBean("engine");  // byName
+////        Engine engine = (Engine) ac.getBean(Engine.class);  // byType - 같은 타입이 3개라서 에러 발생
+//        Door door = (Door) ac.getBean("door");
 
-        Engine engine = (Engine) ac.getBean("engine");
-        Door door = (Door) ac.getBean("door");
+//        car.setColor("red");
+//        car.setOil(100);
+//        car.setEngine(engine);
+//        car.setDoors(new Door[] {ac.getBean("door", Door.class), (Door) ac.getBean("door")});
 
         System.out.println("car = " + car);
     }
