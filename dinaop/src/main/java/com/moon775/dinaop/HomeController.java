29411d709ba3c65373b3d1845f9b21ac2ae656de
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-17 22:19:48 +0900

    Root Web AC(ApplicationContext) 와 Servlet Web AC 의 호출과 기본적 메서드 예시 - 빈 충돌로 인해 servlet-context 수정

--- dinaop/src/main/java/com/moon775/dinaop/HomeController.java
+++ dinaop/src/main/java/com/moon775/dinaop/HomeController.java
@@ -1,39 +1,39 @@
 package com.moon775.dinaop;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-/**
- * Handles requests for the application home page.
- */
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Locale;
+
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	WebApplicationContext servletAC; // Servlet AC
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Locale locale, HttpServletRequest request, Model model) {
+		// 원래는 request.getServletContext()지만, 컨트롤러는 HttpServlet을 상속받지 않아서 아래와 같이 해야함.
+		ServletContext sc = request.getSession().getServletContext(); // ApplicationContextFacade
+		WebApplicationContext rootAC = WebApplicationContextUtils.getWebApplicationContext(sc); // Root AC
+
+		System.out.println("webApplicationContext = " + rootAC);
+		System.out.println("servletAC = " + servletAC);
+
+		System.out.println("rootAC.getBeanDefinitionNames() = " + Arrays.toString(rootAC.getBeanDefinitionNames()));
+		System.out.println("servletAC.getBeanDefinitionNames() = " + Arrays.toString(servletAC.getBeanDefinitionNames()));
+
+		System.out.println("rootAC.getBeanDefinitionCount() = " + rootAC.getBeanDefinitionCount());
+		System.out.println("servletAC.getBeanDefinitionCount() = " + servletAC.getBeanDefinitionCount());
+
+		System.out.println("servletAC.getParent()==rootAC = " + (servletAC.getParent() == rootAC)); // servletAC.getParent()==rootAC = true
 		return "home";
 	}
-	
 }
